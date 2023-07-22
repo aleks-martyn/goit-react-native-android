b06a77dac9d2d13bf382a1365e3e1ee1f8ec3ffa
Oleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-07-22 22:11:33 +0300

    Create Home.js

---
+++ android-project/src/Screens/Home/Home.js
@@ -0,0 +1,17 @@
+import { useNavigation } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Ionicons, Feather } from "@expo/vector-icons";
+import { TouchableOpacity } from "react-native-gesture-handler";
+import { useDispatch } from "react-redux";
+import PostsScreen from "../PostsScreen/PostsScreen";
+import ProfileScreen from "../ProfileScreen/ProfileScreen";
+import CreatePostsScreen from "../CreatePostsScreen/CreatePostsScreen";
+
+const Tab = createBottomTabNavigator();
+
+export default function Home() {
+  const navigation = useNavigation();
+  const dispatch = useDispatch();
+
+  return <></>;
+}
