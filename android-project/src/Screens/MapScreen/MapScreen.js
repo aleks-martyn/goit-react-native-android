32798da40de90ede7b73cf2a4a22d30eb163092f
Oleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-07-22 21:55:26 +0300

    Create MapScreen.js

---
+++ android-project/src/Screens/MapScreen/MapScreen.js
@@ -0,0 +1,9 @@
+import { StyleSheet, View, Dimensions } from "react-native";
+import { useRoute } from "@react-navigation/native";
+import MapView, { Marker } from "react-native-maps";
+
+export default function MapScreen() {
+  const route = useRoute();
+
+  return <View></View>;
+}
