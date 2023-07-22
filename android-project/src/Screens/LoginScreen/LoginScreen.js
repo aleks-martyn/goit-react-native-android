80b4f889bed4a450fa76a6956cf1813341ea9ed9
Oleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-07-22 20:24:41 +0300

    Create LoginScreen.js

---
+++ android-project/src/Screens/LoginScreen/LoginScreen.js
@@ -0,0 +1,15 @@
+import {
+  ImageBackground,
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableWithoutFeedback,
+  Keyboard,
+  TouchableOpacity,
+  View,
+  Alert,
+} from 'react-native';
+import { useDispatch } from 'react-redux';
+import { useNavigation } from '@react-navigation/native';
+import { useState } from 'react';
