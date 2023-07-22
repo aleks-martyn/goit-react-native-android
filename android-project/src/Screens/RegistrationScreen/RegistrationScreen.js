b6b4a81a6e1a00e03a2e3eefe09e29e37edc4ec9
Oleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-07-22 20:09:48 +0300

    Create RegistrationScreen.js

---
+++ android-project/src/Screens/RegistrationScreen/RegistrationScreen.js
@@ -0,0 +1,23 @@
+import {
+  ImageBackground,
+  Image,
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableWithoutFeedback,
+  Keyboard,
+  TouchableOpacity,
+  View,
+  Alert,
+} from 'react-native';
+import { useDispatch } from 'react-redux';
+import { useNavigation } from '@react-navigation/native';
+import { useState } from 'react';
+
+export default function RegistrationScreen() {
+    return (
+        <TouchableWithoutFeedback></TouchableWithoutFeedback>
+    )
+ }
+
