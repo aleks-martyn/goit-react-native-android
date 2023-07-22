bf5868570c2749288e199356a0a7e3ee00c2c8f7
Oleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-07-22 22:00:03 +0300

    Create PostsScreen.js

---
+++ android-project/src/Screens/PostsScreen/PostsScreen.js
@@ -0,0 +1,28 @@
+import React, { useState, useEffect, useCallback } from "react";
+import {
+  View,
+  StyleSheet,
+  Text,
+  SafeAreaView,
+  FlatList,
+  Image,
+  TouchableOpacity,
+} from "react-native";
+import {
+  useNavigation,
+  useRoute,
+  useFocusEffect,
+} from "@react-navigation/native";
+import { Feather } from "@expo/vector-icons";
+import { useDispatch, useSelector } from "react-redux";
+
+export default function PostsScreen() {
+  const navigation = useNavigation();
+  const dispatch = useDispatch();
+
+  return (
+    <View>
+      <Text>Posts Screen</Text>
+    </View>
+  );
+}
