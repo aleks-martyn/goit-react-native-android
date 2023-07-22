cbb5504093d2507fb6a3a8030eb4bd5dc124bc25
Oleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-07-22 21:43:08 +0300

    Update App.js

--- android-project/App.js
+++ android-project/App.js
@@ -2,13 +2,12 @@ import "react-native-gesture-handler";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
 import { useFonts } from "expo-font";
-
+import { View,Text } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <View>
       <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
     </View>
   );
 }
