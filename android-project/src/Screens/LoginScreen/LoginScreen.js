c9e2851e0ca23c596510ce3c4a75f2134058e106
Oleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-07-23 13:25:05 +0300

    Update LoginScreen.js

--- android-project/src/Screens/LoginScreen/LoginScreen.js
+++ android-project/src/Screens/LoginScreen/LoginScreen.js
@@ -13,13 +13,163 @@ import {
 import { useDispatch } from "react-redux";
 import { useNavigation } from "@react-navigation/native";
 import { useState } from "react";
+import BgImage from "../../images/PhotoBG.jpg";
 
 export default function LoginScreen() {
+  const navigation = useNavigation();
+
+  const [focusMail, setFocusMail] = useState(false);
+  const [focusPass, setFocusPass] = useState(false);
+
+  const customStyleMail = focusMail ? styles.inputFocus : styles.input;
+  const customStylePass = focusPass ? styles.inputFocus : styles.input;
+
+  const [secureTextEntry, setSecureTextEntry] = useState(true);
+  const [showText, setShowText] = useState("Показати");
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(null);
+
+  const onPressShowBtn = () => {
+    if (password.trim() !== "" && secureTextEntry) {
+      setSecureTextEntry(false);
+      setShowText("Приховати");
+    } else {
+      setSecureTextEntry(true);
+      setShowText("Показати");
+    }
+  };
+
+  const onLogin = () => {
+    console.log("Log In");
+  };
+
+  const onRegistration = () => navigation.navigate("Registration");
+
   return (
     <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-      <View>
-        <Text>Login Screen</Text>
+      <View style={styles.wrapper}>
+        <ImageBackground
+          source={BgImage}
+          resizeMode="cover"
+          style={styles.image}
+        >
+        <View style={styles.wrap}>
+          <Text style={styles.title}>Увійти</Text>
+          <View style={styles.form}>
+            <TextInput
+              style={[styles.input, customStyleMail]}
+              placeholder="Адреса електронної пошти"
+              value={email}
+              onChangeText={setEmail}
+              onFocus={() => setFocusMail(true)}
+              onBlur={() => setFocusMail(false)}
+              inputMode="email"
+            />
+            <TextInput
+              style={[styles.input, customStylePass]}
+              placeholder="Пароль"
+              value={password}
+              onChangeText={setPassword}
+              onFocus={() => setFocusPass(true)}
+              onBlur={() => {
+                setFocusPass(false);
+                setSecureTextEntry(true);
+                setShowText("Показати");
+              }}
+              secureTextEntry={secureTextEntry}
+            />
+            <TouchableOpacity style={styles.button} onPress={onLogin}>
+              <Text style={styles.textButton}>Увійти</Text>
+            </TouchableOpacity>
+          </View>
+          <TouchableOpacity style={styles.showBtn} onPress={onPressShowBtn}>
+            <Text style={styles.textShowBtn}>{showText}</Text>
+          </TouchableOpacity>
+          <TouchableOpacity onPress={onRegistration}>
+            <Text style={styles.textShowBtn}>
+              Немає акаунту? Зареєструватися
+            </Text>
+          </TouchableOpacity>
+          </View>
+          </ImageBackground>
       </View>
     </TouchableWithoutFeedback>
   );
 }
+
+const styles = StyleSheet.create({
+  wrapper: {
+    width: "100%",
+    height: "100%",
+  },
+  image: {
+    width: "100%",
+    height: "100%",
+    flex: 1,
+    flexDirection: "column",
+    justifyContent: "flex-end",
+  },
+  wrap: {
+    width: "100%",
+    height: 420,
+    paddingTop: 15,
+    paddingHorizontal: 16,
+    backgroundColor: "#fff",
+    borderTopLeftRadius: 25,
+    borderTopRightRadius: 25,
+    alignItems: "center",
+  },
+  title: {
+    marginBottom: 15,
+    fontSize: 30,
+    letterSpacing: 0.01,
+    lineHeight: 35.16,
+    textAlign: "center",
+    color: "#212121",
+  },
+  form: {
+    position: "relative",
+  },
+  input: {
+    height: 35,
+    paddingHorizontal: 16,
+    marginBottom: 10,
+    backgroundColor: "#f6f6f6",
+    borderColor: "#e8e8e8",
+    borderStyle: "solid",
+    borderWidth: 1,
+    borderRadius: 8,
+    fontSize: 16,
+    lineHeight: 19,
+  },
+  inputFocus: {
+    borderColor: "#ff6c00",
+  },
+  button: {
+    height: 40,
+    marginTop: 25,
+    marginBottom: 16,
+    alignItems: "center",
+    backgroundColor: "#ff6c00",
+    paddingVertical: 10,
+    paddingHorizontal: 80,
+    borderRadius: 100,
+  },
+  textButton: {
+    color: "#fff",
+    fontSize: 16,
+    lineHeight: 19,
+  },
+  showBtn: {
+    position: "absolute",
+    top: 54,
+    right: 18,
+  },
+  textShowBtn: {
+    fontSize: 16,
+    lineHeight: 19,
+    color: "#1B4371",
+  },
+});
