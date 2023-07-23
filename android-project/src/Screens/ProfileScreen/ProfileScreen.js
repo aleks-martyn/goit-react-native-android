444f64fe5658eb4571e3b5d77a3797e7dcce6d5c
Oleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-07-23 22:30:08 +0300

    Update ProfileScreen.js

--- android-project/src/Screens/ProfileScreen/ProfileScreen.js
+++ android-project/src/Screens/ProfileScreen/ProfileScreen.js
@@ -19,11 +19,22 @@ export default function ProfileScreen() {
 
   return (
     <View style={styles.container}>
-      <ImageBackground
-        source={BgImage}
-        resizeMode="cover"
-        style={styles.image}
-      ></ImageBackground>
+      <ImageBackground source={BgImage} resizeMode="cover" style={styles.image}>
+        <View style={styles.wrap}>
+          <View style={styles.userPicture}></View>
+          <TouchableOpacity
+            onPress={console.log("log out")}
+            style={styles.logOutBtn}
+          >
+            <Feather
+              name="log-out"
+              size={24}
+              color="#bdbdbd"
+              backgroundColor="#fff"
+            />
+          </TouchableOpacity>
+        </View>
+      </ImageBackground>
     </View>
   );
 }
@@ -42,5 +53,24 @@ const styles = StyleSheet.create({
     flex: 1,
     flexDirection: "column",
     justifyContent: "flex-end",
+  },
+  wrap: {
+    position: "relative",
+    width: "100%",
+    height: 400,
+    paddingTop: 70,
+    paddingHorizontal: 16,
+    backgroundColor: "#fff",
+    borderTopLeftRadius: 25,
+    borderTopRightRadius: 25,
+  },
+  userPicture: {
+    position: "absolute",
+    top: -60,
+    left: 130,
+    width: 120,
+    height: 120,
+    backgroundColor: "#f6f6f6",
+    borderRadius: 16,
   },
 });
