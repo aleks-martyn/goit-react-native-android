e678867b9cba461f832941bf99eb0a0850d0beb2
Oleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-07-23 20:22:01 +0300

    Update Home.js

--- android-project/src/Screens/Home/Home.js
+++ android-project/src/Screens/Home/Home.js
@@ -2,16 +2,115 @@ import { useNavigation } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { Ionicons, Feather } from "@expo/vector-icons";
 import { TouchableOpacity } from "react-native-gesture-handler";
-import { useDispatch } from "react-redux";
+//import { useDispatch } from "react-redux";
 import PostsScreen from "../PostsScreen/PostsScreen";
 import ProfileScreen from "../ProfileScreen/ProfileScreen";
 import CreatePostsScreen from "../CreatePostsScreen/CreatePostsScreen";
 
 const Tab = createBottomTabNavigator();
 
+const styleScreens = {
+  headerStyle: {
+    height: 60,
+    backgroundColor: "#fff",
+    shadowOffset: {
+      width: 0,
+      height: 0.5,
+    },
+    shadowOpacity: 0.3,
+    shadowColor: "#000",
+    shadowRadius: 0,
+  },
+  tabBarStyle: {
+    height: 83,
+    backgroundColor: "#fff",
+    shadowOffset: {
+      width: 0,
+      height: -0.5,
+    },
+    shadowOpacity: 0.3,
+    shadowColor: "#000",
+    shadowRadius: 0,
+  },
+  tabBarShowLabel: false,
+  tabBarActiveBackgroundColor: "#FF6C00",
+  tabBarInactiveTintColor: "#212121",
+  tabBarActiveTintColor: "#fff",
+};
+
 export default function Home() {
   const navigation = useNavigation();
-//  const dispatch = useDispatch();
+  //  const dispatch = useDispatch();
 
-  return <></>;
+  return (
+    <Tab.Navigator initialRouteName="Posts" screenOptions={styleScreens}>
+      <Tab.Screen
+        name="Posts"
+        component={PostsScreen}
+        options={{
+          title: "Публікації",
+          headerRight: () => (
+            <TouchableOpacity onPress={console.log("log out")}>
+              <Feather.Button
+                name="log-out"
+                size={24}
+                color="#bdbdbd"
+                backgroundColor="#fff"
+              />
+            </TouchableOpacity>
+          ),
+          tabBarIcon: ({ color }) => (
+            <Feather name="grid" size={24} color={color} />
+          ),
+          tabBarItemStyle: {
+            height: 50,
+            marginTop: 9,
+            marginRight: 10,
+            marginLeft: 20,
+          },
+        }}
+      />
+      <Tab.Screen
+        name="CreatePosts"
+        component={CreatePostsScreen}
+        options={{
+          title: "Створити публікацію",
+          headerLeft: () => (
+            <TouchableOpacity onPress={() => navigation.navigate("Posts")}>
+              <Feather.Button
+                name="arrow-left"
+                size={24}
+                color="#212121"
+                backgroundColor="#fff"
+              />
+            </TouchableOpacity>
+          ),
+          tabBarIcon: ({ color }) => (
+            <Ionicons name="add" size={24} color={color} />
+          ),
+          tabBarItemStyle: {
+            height: 50,
+            marginTop: 9,
+            marginHorizontal: 5,
+          },
+        }}
+      />
+      <Tab.Screen
+        name="Profile"
+        component={ProfileScreen}
+        options={{
+          headerShown: false,
+          tabBarIcon: ({ color }) => (
+            <Feather name="user" size={24} color={color} />
+          ),
+          tabBarItemStyle: {
+            height: 50,
+            marginTop: 9,
+            marginRight: 20,
+            marginLeft: 10,
+          },
+        }}
+      />
+    </Tab.Navigator>
+  );
 }
