97d052c914020b9b566261d96bdb09e6f29bcbf3
Oleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-07-23 20:22:14 +0300

    Update PostsScreen.js

--- android-project/src/Screens/PostsScreen/PostsScreen.js
+++ android-project/src/Screens/PostsScreen/PostsScreen.js
@@ -3,7 +3,6 @@ import {
   View,
   StyleSheet,
   Text,
-  SafeAreaView,
   FlatList,
   Image,
   TouchableOpacity,
@@ -14,15 +13,27 @@ import {
   useFocusEffect,
 } from "@react-navigation/native";
 import { Feather } from "@expo/vector-icons";
-import { useDispatch, useSelector } from "react-redux";
+//import { useDispatch, useSelector } from "react-redux";
 
 export default function PostsScreen() {
   const navigation = useNavigation();
-//  const dispatch = useDispatch();
+  //  const dispatch = useDispatch();
 
   return (
-    <View>
+    <View style={styles.container}>
       <Text>Posts Screen</Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    width: "100%",
+    height: "100%",
+    flex: 1,
+    alignItems: "center",
+    paddingTop: 10,
+    paddingHorizontal: 16,
+    backgroundColor: "#fff",
+  },
+});
