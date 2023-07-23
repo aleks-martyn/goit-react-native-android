1fdc3e3792f8c69103d646d173c25d11ebba7c53
Oleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-07-23 10:15:18 +0300

    Create firebase.js

---
+++ android-project/config/firebase.js
@@ -0,0 +1,16 @@
+import { initializeApp } from 'firebase/app';
+import 'firebase/auth';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyCqBUoPniFFbwK17TCKYMpmhNMnVOeiLws',
+  authDomain:'posts-9d0f3.firebaseapp.com',
+  projectId: 'posts-9d0f3',
+  storageBucket: 'posts-9d0f3.appspot.com',
+  messagingSenderId: '1098182767392',
+  appId: '1:1098182767392:web:2b10c6a554b841586b9d45',
+  measurementId: 'G-3C9KNLM62Q',
+};
+
+const app = initializeApp(firebaseConfig);
+
+export default app;
