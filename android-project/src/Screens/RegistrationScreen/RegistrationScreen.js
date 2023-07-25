dcc69466302696bc14073e2b21ba47e36977dffe
Oleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-07-25 19:41:16 +0300

    Update RegistrationScreen.js

--- android-project/src/Screens/RegistrationScreen/RegistrationScreen.js
+++ android-project/src/Screens/RegistrationScreen/RegistrationScreen.js
@@ -210,7 +210,7 @@ const styles = StyleSheet.create({
     backgroundColor: "#ff6c00",
     paddingVertical: 10,
     paddingHorizontal: 80,
-    borderRadius: 100,
+    borderRadius: 50,
   },
   textButton: {
     color: "#fff",
