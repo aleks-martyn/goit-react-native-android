769cff03f28abb9816cf44787d46d7a67da2be80
Oleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-07-25 16:05:05 +0300

    Create store.js

---
+++ android-project/src/redux/store.js
@@ -0,0 +1,34 @@
+import { configureStore } from "@reduxjs/toolkit";
+import {
+  persistReducer,
+  persistStore,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { authReducer } from "./auth/authSlice";
+
+const persistConfig = {
+  key: "auth",
+  storage: AsyncStorage,
+};
+
+export const store = configureStore({
+  reducer: {
+    auth: persistReducer(persistConfig, authReducer),
+    //    posts: postsReducer,
+    //    comments: commentsReducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
+
+export const persistor = persistStore(store);
