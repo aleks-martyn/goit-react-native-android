bceab301405c8a3fc9119381d0405161f773f168
Oleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-07-25 17:33:03 +0300

    Update store.js

--- android-project/src/redux/store.js
+++ android-project/src/redux/store.js
@@ -11,6 +11,7 @@ import {
 } from "redux-persist";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { authReducer } from "./auth/authSlice";
+import { postsReducer } from './posts/postsSlice';
 
 const persistConfig = {
   key: "auth",
@@ -20,7 +21,7 @@ const persistConfig = {
 export const store = configureStore({
   reducer: {
     auth: persistReducer(persistConfig, authReducer),
-    //    posts: postsReducer,
+    posts: postsReducer,
     //    comments: commentsReducer,
   },
   middleware: (getDefaultMiddleware) =>
