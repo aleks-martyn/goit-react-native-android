0d348dd3a8dcb053a7e4985fae26b8090afe5f46
Oleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-07-26 18:17:31 +0300

    Update MapScreen.js

--- android-project/src/Screens/MapScreen/MapScreen.js
+++ android-project/src/Screens/MapScreen/MapScreen.js
@@ -5,5 +5,41 @@ import MapView, { Marker } from "react-native-maps";
 export default function MapScreen() {
   const route = useRoute();
 
-  return <View></View>;
+  return (
+    <View>
+      <MapView
+        style={styles.mapStyle}
+        region={{
+          latitude: route.params.latitude,
+          longitude: route.params.longitude,
+        }}
+        mapType="standard"
+        minZoomLevel={5}
+      >
+        <Marker
+          title="I am here"
+          coordinate={{
+            latitude: route.params.latitude,
+            longitude: route.params.longitude,
+          }}
+          description="Hello"
+        />
+      </MapView>
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    width: "100%",
+    height: "100%",
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#fff",
+  },
+  mapStyle: {
+    width: Dimensions.get("window").width,
+    height: Dimensions.get("window").height,
+  },
+});
