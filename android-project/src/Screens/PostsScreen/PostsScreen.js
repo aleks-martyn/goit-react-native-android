ca0b70e4dd65d373437474482b798cad9d178c09
Oleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-07-26 18:18:20 +0300

    Update PostsScreen.js

--- android-project/src/Screens/PostsScreen/PostsScreen.js
+++ android-project/src/Screens/PostsScreen/PostsScreen.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useCallback } from "react";
+import React, { useCallback } from "react";
 import {
   View,
   StyleSheet,
@@ -9,19 +9,65 @@ import {
 } from "react-native";
 import {
   useNavigation,
-  useRoute,
   useFocusEffect,
 } from "@react-navigation/native";
 import { Feather } from "@expo/vector-icons";
-//import { useDispatch, useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { selectPosts } from "../../redux/posts/postsSelectors";
+import { getAllPosts } from "../../redux/posts/postsOperations";
 
 export default function PostsScreen() {
   const navigation = useNavigation();
-  //  const dispatch = useDispatch();
+  const dispatch = useDispatch();
+  const posts = useSelector(selectPosts);
+
+  useFocusEffect(
+    useCallback(() => {
+      dispatch(getAllPosts()).unwrap();
+    }, [dispatch])
+  );
 
   return (
     <View style={styles.container}>
-      <Text>Posts Screen</Text>
+      <FlatList
+        data={posts}
+        keyExtractor={(_, index) => index.toString()}
+        renderItem={({ item }) => (
+          <View style={styles.itemWrap}>
+            <Image source={{ uri: item.photo }} style={styles.photo} />
+            <View style={styles.nameWrap}>
+              <Text>{item.name}</Text>
+              <Text>{item.displayName}</Text>
+            </View>
+            <View style={styles.infoWrap}>
+              <TouchableOpacity
+                style={styles.commentsWrap}
+                onPress={() =>
+                  navigation.navigate("Comments", {
+                    uri: item.photo,
+                    id: item.id,
+                  })
+                }
+              >
+                <Feather name="message-circle" size={24} color={"#bdbdbd"} />
+                <Text style={styles.commentsText}>Коментарі</Text>
+              </TouchableOpacity>
+              <TouchableOpacity
+                style={styles.locationWrap}
+                onPress={() =>
+                  navigation.navigate("Map", {
+                    latitude: item.location.coords.latitude,
+                    longitude: item.location.coords.longitude,
+                  })
+                }
+              >
+                <Feather name="map-pin" size={24} color="#bdbdbd" />
+                <Text style={styles.nameLocationText}>{item.nameLocation}</Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+        )}
+      />
     </View>
   );
 }
@@ -32,7 +78,7 @@ const styles = StyleSheet.create({
     height: "100%",
     flex: 1,
     alignItems: "center",
-    paddingTop: 10,
+    paddingTop: 20,
     paddingHorizontal: 16,
     backgroundColor: "#fff",
   },
@@ -41,39 +87,39 @@ const styles = StyleSheet.create({
     marginBottom: 34,
   },
   photo: {
-    width: 288,
-    height: 180,
+    width: 343,
+    height: 240,
     marginBottom: 8,
     borderRadius: 8,
   },
   infoWrap: {
     flex: 1,
-    flexDirection: 'row',
-    justifyContent: 'space-between',
+    flexDirection: "row",
+    justifyContent: "space-between",
   },
   commentsWrap: {
     flex: 1,
-    flexDirection: 'row',
-    alignItems: 'center',
+    flexDirection: "row",
+    alignItems: "center",
     gap: 5,
   },
   locationWrap: {
     flex: 1,
-    flexDirection: 'row',
-    alignItems: 'center',
+    flexDirection: "row",
+    alignItems: "center",
     gap: 5,
-    justifyContent: 'flex-end',
+    justifyContent: "flex-end",
   },
   nameWrap: {
     flex: 1,
     marginBottom: 11,
-    flexDirection: 'row',
-    justifyContent: 'space-between',
+    flexDirection: "row",
+    justifyContent: "space-between",
   },
   commentsText: {
-    color: '#212121',
+    color: "#212121",
   },
   nameLocationText: {
-    color: '#212121',
+    color: "#212121",
   },
 });
